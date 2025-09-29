f5cf54fc3a7bf554bec99f6e8385d5b942b400b4
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2025-09-29 13:23:35 +0330

    [learnt] Neocortex TTS (but it is not local)

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,6 +4,11 @@ using Neocortex;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource;
+
+    [Header("Neocortex Audio Components")]
+    [SerializeField] private NeocortexSmartAgent agent;
+    [Header("Ollama Text Components")]
     [SerializeField] private OllamaModelDropdown modelDropdown;
     [SerializeField, TextArea] private string systemPrompt;
 
@@ -19,13 +24,23 @@ public class Test : MonoBehaviour
 
         request.AddSystemMessage(systemPrompt);
 
+        agent.OnAudioResponseReceived.AddListener(OnAudioResponseReceived);
+
         request.Send("Headshot");
         Debug.Log("Headshot sent");
-        
+
     }
 
     private void OnChatResponseReceived(ChatResponse response)
     {
         Debug.Log(response.message);
+
+        agent.TextToAudio(response.message);
+    }
+
+    private void OnAudioResponseReceived(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 }
